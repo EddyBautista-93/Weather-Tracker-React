07aeeda922af3291b363b6ae31868ae12dae0427
EddyBautista <bautista.j.eddy@gmail.com>
2020-11-26 10:05:30 -0600

    added com folder and .env

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import GoogleMapReact from 'google-map-react'
+
+const Map = ({center, zoom}) => {
+    return (
+        <div className="map">
+            <GoogleMapReact>
+                
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+export default maM
